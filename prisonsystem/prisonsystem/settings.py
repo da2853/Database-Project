7510919aadbf9385f59028cc494efc1f2f9dfa59
Danyal Ahmad <da2853@nyu.edu>
2024-03-30 22:03:25 -0400

    static changes

--- prisonsystem/prisonsystem/settings.py
+++ prisonsystem/prisonsystem/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Add the path to your static files
 STATICFILES_DIRS = [
